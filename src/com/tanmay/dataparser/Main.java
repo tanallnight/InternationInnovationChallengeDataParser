4f29ef179498d8e1150f09bc81b8ab8ba6a3bb97
Tanmay Parikh <tanpar1995@gmail.com>
2016-10-12 00:23:54 -0400

    Cleanup

--- src/com/tanmay/dataparser/Main.java
+++ src/com/tanmay/dataparser/Main.java
@@ -1,33 +1,32 @@
-package com.tanmay;
+package com.tanmay.dataparser;
 
 import java.io.*;
 
 public class Main {
 
-    private static BufferedReader bufferedReader;
-    private static FileWriter fileWriter;
-
     public static void main(String[] args) {
 
         File parentFolder = new File("F:/Scotia");
         File[] files = parentFolder.listFiles();
+        long startTime;
 
         for (File file : files) {
             if (file.isFile() && !file.getName().contains("txt")) {
+                startTime = System.currentTimeMillis();
+                System.out.print("Parsing file: " + file.getName() + "\t\t\t\t\t");
                 String filePath = file.getPath();
-                String outFile = "F:/Scotia/Quick/" + file.getName().replace("del", "csv");
+                String outFile = "F:/Scotia/Outputs/" + file.getName().replace("del", "csv");
                 String delimiter = "\\x01";
                 String line;
 
                 long lineNum = 0;
                 try {
-                    bufferedReader = new BufferedReader(new FileReader(filePath));
-                    fileWriter = new FileWriter(outFile);
+                    BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+                    FileWriter fileWriter = new FileWriter(outFile);
                     while ((line = bufferedReader.readLine()) != null) {
-                        if(lineNum >= 10000) {
+                        if(lineNum > 1000000) {
                             break;
                         }
-                        System.out.println(lineNum);
                         lineNum++;
                         String[] currentLine = line.split(delimiter);
                         String newLine = "";
@@ -35,7 +34,6 @@ public class Main {
                         for (String s : currentLine) {
                             newLine = newLine + separator + s;
                             separator = ",";
-                            //System.out.println(newLine);
                         }
                         fileWriter.append(newLine);
                         fileWriter.append("\n");
@@ -46,7 +44,10 @@ public class Main {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                long operationTime = System.currentTimeMillis() - startTime;
+                System.out.println(operationTime + "ms");
             }
         }
+        System.out.println("DONE!");
     }
 }
